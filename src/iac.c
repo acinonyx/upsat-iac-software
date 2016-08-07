d88047707a5f1d97ba08d8c8171be3fb6c1bc947
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2016-08-07 21:33:25 +0300

    Transfer and evaluate response from OBC after block transfer
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- src/iac.c
+++ src/iac.c
@@ -352,8 +352,15 @@ static int transfer_tiles(MagickWand ***wands, const config_t *config)
                                          packet.buf,
                                          (uint32_t) packet.size) == IAC_FAILURE)
                         return IAC_FAILURE;
-                    resp = packet.buf[0];
                     free(packet.buf);
+                    /* Transfer response */
+                    resp = 0;
+                    IAC_VERBOSE("Transferring response...\n");
+                    if (iac_spi_transfer(fd,
+                                         &resp,
+                                         sizeof(resp)) == IAC_FAILURE)
+                        return IAC_FAILURE;
+                    IAC_VERBOSE("Response from OBC is 0x%#X...\n", resp);
                 } while (resp != IAC_OBC_BLOCK_ACK);
                 /* Next block */
                 block.data += IAC_OBC_BLOCK_SIZE;
